5f4cda4dec821c6ce102d609983c04c1d97d0e0c
mshahzebraza97 <m.shahzebraza.97@gmail.com>
2020-07-07 00:30:43 +0500

    Returning Values: Big Fixed
    Lesson: Always make sure that returning value reaches the desired Execution Context where it is required to be used or displayed, especially when NESTED functions are bein used....

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ var UIController = ( function () {
 var AppController = ( function (budgetCtrl , UICtrl) {
     
     var mData = function (q) {
-        budgetCtrl.publicTest(q);
+        return budgetCtrl.publicTest(q);
     }
 
     var mUI = function () {
@@ -52,10 +52,14 @@ var AppController = ( function (budgetCtrl , UICtrl) {
             // mData(); // runs mData = runs publicTest and demands a NUMBER argument // = returns added value from its Data module //nothing is logged
                 // Does not work bcz mData is linked with publictest which demands argument, whereas mData does not pass in any argument. It simply does not have the capacity to pass in the argument as we can't directly call it, this ability must be transferred from top to bottom of the chain, starting from fetchFromData
         
-            // mData(q); // also does not work bcz AppController.fetchFromData(q) simply runs the fetchDromData method and not the function inside it.. 
+            mData(q); // also does not work bcz AppController.fetchFromData(q) simply runs the fetchDromData method and not the function inside it.. This command here will only "get the returned value" but "will not" pass it on/Return it towards others
+            
+            return mData(q) // Done!!!
+            // Lesson Learnt: always complete the return value cycle, whenever you use nested loops & make sure the returning value keeps on returning till the desired Execution context is approached!!!
 
+            
             // console.log("This is simply:"+q);
-            console.log(mData(1));
+            // console.log(mData(1));
 
         }
     }
