70293928c8c7283c8e37f2a6d99677d486e72453
mshahzebraza97 <m.shahzebraza.97@gmail.com>
2020-07-07 23:20:01 +0500

    Get Inputs & DOMstrings

--- app.js
+++ app.js
@@ -1,67 +1,79 @@
+// BUDGET CONTROLLER
 var budgetController = ( function () {
 
-    var x = 10;
-    function add(a) {
-        return x + a;
-    }
-
-    return {
-        publicTest: function (z) {
-            return add(z);
-        }
-    }
+    // Some Code
         
 })()
 
 
+// UI CONTROLLER
 var UIController = ( function () {
-    
     function acceptCall() {
-        console.log("Someone called me from UI Module");
+
+    var DOMstrings = {
+        inputType : ".add__type",
+        inputDescription : ".add__description",
+        inputValue : ".add__value",
+        inputBtn : ".add__btn"
+    }
+
+
+
+    // PUBLIC METHODS    
+     
+    return { 
+        getInput : function () {
+            return {
+                type : document.querySelector(DOMstrings.inputType).value,
+                description : document.querySelector(DOMstrings.inputDescription).value,
+                value : document.querySelector(DOMstrings.inputValue).value
+                // btn : document.querySelector(".add__btn").value,
+            }
+        },
+
+        getDOMstrings : function () {
+            return DOMstrings; 
+        }
+        
     }
     
-    return {
-        someMethod: function () {
-            acceptCall();
-        }
-    }
-        
 })()
 
 
 
+// GLOBAL APP CONTROLLER
 var AppController = ( function (budgetCtrl , UICtrl) {
     
-    var mData = function (q) {
-        return budgetCtrl.publicTest(q);
-    }
+    var DOM = UICtrl.getDOMstrings(); // recieves DOM strings from UICtrl
+    
+    var ctrlAddItem = function(){
 
-    var mUI = function () {
-        // UICtrl.acceptCall();
-        UICtrl.someMethod();
+        // Get Input
+        var input = UICtrl.getInput();
+        console.log(input);
+
+        
+        // Clear Fields
+
+        // Update Income/Expenses
+
+        // Show Income / Expenses
+
+        // Update Budget
     }
     
-    return {
 
-        fetchFromUI: function () {
-            mUI(); // runs mUI = runs someMethod // = runs acceptCall // = logs string
-        },
-        
-        fetchFromData: function (q) {
+    // FOR EVENT LISTENERS
+    document.querySelector(DOM.inputBtn).addEventListener("click" , ctrlAddItem)
 
-            // mData(); // runs mData = runs publicTest and demands a NUMBER argument // = returns added value from its Data module //nothing is logged
-                // Does not work bcz mData is linked with publictest which demands argument, whereas mData does not pass in any argument. It simply does not have the capacity to pass in the argument as we can't directly call it, this ability must be transferred from top to bottom of the chain, starting from fetchFromData
-        
-            mData(q); // also does not work bcz AppController.fetchFromData(q) simply runs the fetchDromData method and not the function inside it.. This command here will only "get the returned value" but "will not" pass it on/Return it towards others
-            
-            return mData(q) // Done!!!
-            // Lesson Learnt: always complete the return value cycle, whenever you use nested loops & make sure the returning value keeps on returning till the desired Execution context is approached!!!
+    document.addEventListener("keypress", function (event) {
 
-            
-            // console.log("This is simply:"+q);
-            // console.log(mData(1));
-
+        if (event.keyCode === 13 || event.which === 13) {            
+            ctrlAddItem();
         }
-    }
+        
+    })
+     
+    // PUBLIC METHODS    
         
 })(budgetController, UIController)
